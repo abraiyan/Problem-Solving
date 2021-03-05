546dfa25d7ab68e412205c8d474bb8afc8e1e13d
abraiyan <abraiyan20@gmail.com>
2021-03-05 11:50:40 +0600

    isomorphic string added

---
+++ Problem_Solving/LeetCode/IsomorphicString.js
@@ -0,0 +1,42 @@
+/*
+    Problem: Isomorphic Strings (https://leetcode.com/problems/isomorphic-strings/)
+
+    < Input: s = "egg", t = "add"
+    < Output: true
+    < Input: s = "foo", t = "bar"
+    < Output: false
+
+    # Use: HashMap
+    -> Time Complexity: O(n), Space Complexity: O(n)
+*/
+
+var isIsomorphic = function (s, t) {
+  if (s.length !== t.length) return false
+
+  const mapS = new Map()
+  const mapT = new Map()
+
+  let outputS = ''
+  let outputT = ''
+  let counter = 1
+
+  for (let i = 0; i < s.length; i++) {
+    if (!mapS.has(s[i])) {
+      mapS.set(s[i], counter)
+      counter++
+    }
+    outputS = outputS + mapS.get(s[i])
+  }
+
+  counter = 1
+
+  for (let i = 0; i < t.length; i++) {
+    if (!mapT.has(t[i])) {
+      mapT.set(t[i], counter)
+      counter++
+    }
+    outputT = outputT + mapT.get(t[i])
+  }
+
+  return outputS === outputT
+}
