2edc9f356b05b5296d1482a038a1ab79a9d492cd
abraiyan <abraiyan20@gmail.com>
2021-03-15 00:01:56 +0600

    1629 optimized version added

--- Problem_Solving/LeetCode/1629.js
+++ Problem_Solving/LeetCode/1629.js
@@ -5,27 +5,19 @@
 */
 
 var slowestKey = function (releaseTimes, keysPressed) {
-  const map = new Map()
-  map.set(keysPressed[0], releaseTimes[0])
-
   let output = keysPressed[0]
+  let max = releaseTimes[0]
 
   for (let i = 1; i < keysPressed.length; i++) {
     let gap = releaseTimes[i] - releaseTimes[i - 1]
-    if (map.has(keysPressed[i])) {
-      if (map.get(keysPressed[i]) < gap) {
-        map.set(keysPressed[i], gap)
-      }
-    } else {
-      map.set(keysPressed[i], gap)
-    }
 
-    if (map.get(keysPressed[i]) > map.get(output)) {
+    if (gap > max) {
+      max = gap
       output = keysPressed[i]
     }
 
-    if (map.get(keysPressed[i]) === map.get(output)) {
-      output = keysPressed[i] > output ? keysPressed[i] : output
+    if (gap === max) {
+      output = output > keysPressed[i] ? output : keysPressed[i]
     }
   }
 
