4d0b88d0c6565d926af894fe38b3399c64f9949f
abraiyan <abraiyan20@gmail.com>
2021-03-19 01:05:30 +0600

    605 optimized

--- Problem_Solving/LeetCode/605.js
+++ Problem_Solving/LeetCode/605.js
@@ -7,28 +7,11 @@
 var canPlaceFlowers = function (flowerbed, n) {
   let counter = 0
 
-  if (flowerbed.length === 1 && flowerbed[0] === 0) return true
-
   for (let i = 0; i < flowerbed.length; i++) {
-    if (i === 0 && flowerbed[i] === 0 && flowerbed[i + 1] === 0) {
-      counter++
-      i++
-      continue
-    }
-
-    if (
-      i === flowerbed.length - 1 &&
-      flowerbed[i] === 0 &&
-      flowerbed[i - 1] === 0
-    ) {
-      counter++
-      break
-    }
-
     if (
       flowerbed[i] === 0 &&
-      flowerbed[i - 1] === 0 &&
-      flowerbed[i + 1] === 0
+      (i === 0 || flowerbed[i - 1] === 0) &&
+      (i === flowerbed.length - 1 || flowerbed[i + 1] === 0)
     ) {
       counter++
       i++
