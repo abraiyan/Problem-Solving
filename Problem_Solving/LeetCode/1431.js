f28bdcd2947066bc020a6b84b6a2963e9294b1c1
abraiyan <abraiyan20@gmail.com>
2021-03-25 13:07:06 +0600

    1431 added

---
+++ Problem_Solving/LeetCode/1431.js
@@ -0,0 +1,24 @@
+/*
+    Problem: Kids With the Greatest Number of Candies (https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/)
+  
+    -> Time Complexity: O(n), Space Complexity: O(n)
+*/
+
+var kidsWithCandies = function (candies, extraCandies) {
+  let max = candies[0]
+  let output = []
+
+  for (let i = 1; i < candies.length; i++) {
+    if (max < candies[i]) max = candies[i]
+  }
+
+  for (let i = 0; i < candies.length; i++) {
+    if (candies[i] + extraCandies >= max) {
+      output.push(true)
+    } else {
+      output.push(false)
+    }
+  }
+
+  return output
+}
