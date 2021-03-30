66ba3634e89a03aeded90f5075f9a0f2e334413d
abraiyan <abraiyan20@gmail.com>
2021-03-30 12:53:06 +0600

    1365 OPTIMIZED

--- Problem_Solving/LeetCode/1365.js
+++ Problem_Solving/LeetCode/1365.js
@@ -3,22 +3,22 @@
     -> Time Complexity: O(n), Space Complexity: O(n)
 */
 
-var smallerNumbersThanCurrent = function (nums) {
+var smallerNumbersThanCurrent = function(nums) {
   let count = new Array(101).fill(0)
-  let output = new Array(nums.length).fill(0)
+  let output = []
 
   for (let i = 0; i < nums.length; i++) {
     count[nums[i]]++
   }
 
   for (let i = 1; i <= 100; i++) {
-    count[i] += count[i - 1]
+    count[i] += count[i - 1];
   }
 
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] === 0) output[i] = 0
-    else output[i] = count[nums[i] - 1]
+  for(let i = 0; i < nums.length; i++) {
+    if(nums[i] === 0) output.push(0)
+    else output.push(count[nums[i] - 1])
   }
 
   return output
-}
+};
