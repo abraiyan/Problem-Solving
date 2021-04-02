fa532330c28cbd4f76f98b7cda4c05f0064bb8e9
abraiyan <abraiyan20@gmail.com>
2021-04-02 19:49:03 +0600

    1295 added

---
+++ Problem_Solving/LeetCode/1295.js
@@ -0,0 +1,17 @@
+/*
+    Problem: Find Numbers with Even Number of Digits (https://leetcode.com/problems/find-numbers-with-even-number-of-digits/)
+
+    # Array, Math
+    -> Time Complexity: O(n), Space Complexity: O(1)
+
+*/
+
+var findNumbers = function (nums) {
+  let output = 0
+
+  for (const n of nums) {
+    if (parseInt(Math.floor(Math.log10(n)) + 1) % 2 === 0) output++
+  }
+
+  return output
+}
