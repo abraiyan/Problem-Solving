faa6b92a8a5691b71acd5d97fbdb7d6323db6a3e
abraiyan <abraiyan20@gmail.com>
2021-05-28 12:43:16 +0600

    1299 added

---
+++ Problem_Solving/LeetCode/1299.js
@@ -0,0 +1,13 @@
+/*
+    Problem: Replace Elements with Greatest Element on Right Side (https://leetcode.com/problems/replace-elements-with-greatest-element-on-right-side/)
+*/
+
+var replaceElements = function (arr) {
+  let max = -1
+  for (let i = arr.length - 1; i >= 0; i--) {
+    let temp = arr[i]
+    arr[i] = max
+    max = Math.max(max, temp)
+  }
+  return arr
+}
