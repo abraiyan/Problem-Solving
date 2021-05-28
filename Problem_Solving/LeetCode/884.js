1acfe96c2b85d7f48c5e12a17613aa52dc3cec50
abraiyan <abraiyan20@gmail.com>
2021-05-28 07:34:56 +0600

    884 added

---
+++ Problem_Solving/LeetCode/884.js
@@ -0,0 +1,24 @@
+/*
+    Problem: Uncommon Words from Two Sentences (https://leetcode.com/problems/uncommon-words-from-two-sentences/)
+*/
+
+var uncommonFromSentences = function (s1, s2) {
+  let output = []
+  const map = new Map()
+
+  for (const s of s1.split(' ')) {
+    map.set(s, map.has(s) ? map.get(s) + 1 : 1)
+  }
+
+  for (const s of s2.split(' ')) {
+    map.set(s, map.has(s) ? map.get(s) + 1 : 1)
+  }
+
+  for (const key of map.keys()) {
+    if (map.get(key) === 1) {
+      output.push(key)
+    }
+  }
+
+  return output
+}
