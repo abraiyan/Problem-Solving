972f2432bbcbf0ae9c03ec0b61a4f174f7fdada0
abraiyan <abraiyan20@gmail.com>
2021-06-08 23:52:28 +0600

    1720 aded

---
+++ Problem_Solving/LeetCode/1720.java
@@ -0,0 +1,16 @@
+/*
+    Problem: Decode XORed Array (https://leetcode.com/problems/decode-xored-array/)
+*/
+
+class Problem1720 {
+    public int[] decode(int[] encoded, int first) {
+        int[] output = new int[encoded.length + 1];
+        output[0] = first;
+
+        for (int i = 0; i < encoded.length; i++) {
+            output[i + 1] = output[i] ^ encoded[i];
+        }
+
+        return output;
+    }
+}
