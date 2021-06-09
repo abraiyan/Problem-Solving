287a7b533587c24b54966f6c41d831afd8fc2ae6
abraiyan <abraiyan20@gmail.com>
2021-06-09 23:56:25 +0600

    1342 added

---
+++ Problem_Solving/LeetCode/1342.java
@@ -0,0 +1,16 @@
+class Problem1342 {
+    public int numberOfSteps(int num) {
+        int counter = 0;
+        while (num != 0) {
+            if (num % 2 == 0) {
+                num = num / 2;
+                counter++;
+            } else {
+                num = num - 1;
+                counter++;
+            }
+        }
+
+        return counter;
+    }
+}
