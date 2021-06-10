901d111af5fd591c974895a126371037b6c8f573
abraiyan <abraiyan20@gmail.com>
2021-06-10 12:32:52 +0600

    190 added

---
+++ Problem_Solving/LeetCode/190.cpp
@@ -0,0 +1,17 @@
+#include <stdint.h>
+
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t result = 0;
+        int power = 31;
+        
+        while(n != 0) {
+            result += (n & 1) << power;
+            n = n >> 1;
+            power--;
+        }
+        
+        return result;
+    }
+};
