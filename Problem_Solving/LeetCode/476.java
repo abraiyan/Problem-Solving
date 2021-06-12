f7181701e332762058e7678c9478da93056bffe3
abraiyan <abraiyan20@gmail.com>
2021-06-12 08:00:29 +0600

    476 added

--- Problem_Solving/LeetCode/476.java
+++ Problem_Solving/LeetCode/476.java
@@ -1,5 +1,5 @@
 class Solution476 {
     public int findComplement(int num) {
-        return ~num & ((Integer.highestOneBit(num) << 1) - 1);
+        return ~num & ((Integer.highestOneBit(num)) - 1);
     }
 }
