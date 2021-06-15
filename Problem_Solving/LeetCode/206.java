a324eb009ed870a0a4281fbb908ab3f4604048cd
abraiyan <abraiyan20@gmail.com>
2021-06-15 20:07:40 +0600

    141 added

---
+++ Problem_Solving/LeetCode/206.java
@@ -0,0 +1,14 @@
+class Solution141 {
+    public ListNode reverseList(ListNode head) {
+        ListNode previousNode = null;
+    
+        while(head != null) {
+            ListNode temp = head.next;
+            head.next = previousNode;
+            previousNode = head;
+            head = temp;
+        }
+    
+        return previousNode;
+    }
+}
