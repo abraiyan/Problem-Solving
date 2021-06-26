3dd9194b793ab2c6a08e6677299a95b1fa07a640
abraiyan <abraiyan20@gmail.com>
2021-06-26 17:32:33 +0600

    1282 added

--- Problem_Solving/LeetCode/1282.js
+++ Problem_Solving/LeetCode/1282.js
@@ -1,27 +1,18 @@
 var groupThePeople = function (groupSizes) {
   let map = new Map()
-  for (let i = 0; i < groupSizes.length; i++) {
-    if (map.has(groupSizes[i])) {
-      map.set(groupSizes[i], [...map.get(groupSizes[i]), i])
-    } else {
-      map.set(groupSizes[i], [i])
-    }
-  }
-
   let output = []
-  for (const key of map.keys()) {
-    if (key === map.get(key).length) {
-      output.push(map.get(key))
+
+  for (let i = 0; i < groupSizes.length; i++) {
+    let n = groupSizes[i]
+    if (map.has(n)) {
+      map.set(n, [...map.get(n), i])
     } else {
-      let size = map.get(key).length
-      let start = 0
-      let end = start + key
-      let interval = size / key
-      for (let i = 0; i < interval; i++) {
-        output.push(map.get(key).slice(start, end))
-        start = end
-        end = end + key
-      }
+      map.set(n, [i])
+    }
+
+    if (n === map.get(n).length) {
+      output.push(map.get(n))
+      map.set(n, [])
     }
   }
 
