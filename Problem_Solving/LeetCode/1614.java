69f7fa9d971d3549a89a23c129165f84f4e8f945
abraiyan <abraiyan20@gmail.com>
2021-07-01 02:02:38 +0600

    1614 added

--- Problem_Solving/LeetCode/1614.java
+++ Problem_Solving/LeetCode/1614.java
@@ -1,28 +1,21 @@
-import java.util.Stack;
 
 class Solution {
     public int maxDepth(String s) {
         int height = 0;
-        Stack<Character> stack = new Stack<>();
+        int max = 0;
 
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
-            if (c != '(' && c != ')') {
-                continue;
-            }
-
             if (c == '(') {
-                stack.push(c);
-                if (stack.size() > height)
-                    height = stack.size();
-            } else {
-                stack.pop();
+                height++;
+                if (height > max)
+                    max = height;
+            }
+            if (c == ')') {
+                height--;
             }
         }
 
-        if (stack.isEmpty())
-            return height;
-        else
-            return 0;
+        return max;
     }
 }
