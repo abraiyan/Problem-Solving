b895f80b8232a8b8a0e3d63309e39fa2bffb1a2b
abraiyan <abraiyan20@gmail.com>
2021-07-19 20:33:56 +0600

    1710 added

--- Problem_Solving/LeetCode/1710.java
+++ Problem_Solving/LeetCode/1710.java
@@ -1,6 +1,6 @@
 import java.util.Arrays;
 
-class Solution {
+class Solution1710 {
     public int maximumUnits(int[][] boxTypes, int truckSize) {
         Arrays.sort(boxTypes, (a, b) -> b[1] - a[1]);
         int output = 0;
