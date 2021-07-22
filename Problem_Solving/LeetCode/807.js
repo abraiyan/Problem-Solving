714d28361546a4f3e675df918719667e45818588
abraiyan <abraiyan20@gmail.com>
2021-07-22 07:36:14 +0600

    807 edited

--- Problem_Solving/LeetCode/807.js
+++ Problem_Solving/LeetCode/807.js
@@ -3,40 +3,20 @@
  * @return {number}
  */
 var maxIncreaseKeepingSkyline = function (grid) {
-  let rowMap = new Map()
-  let columnMap = new Map()
+  let rowMap = new Array(grid.length).fill(0)
+  let columnMap = new Array(grid.length).fill(0)
   let output = 0
 
   for (let i = 0; i < grid.length; i++) {
     for (let j = 0; j < grid[0].length; j++) {
-      if (rowMap.has(i)) {
-        if (rowMap.get(i) < grid[i][j]) {
-          rowMap.set(i, grid[i][j])
-        }
-      } else {
-        rowMap.set(i, grid[i][j])
-      }
-
-      if (columnMap.has(j)) {
-        if (columnMap.get(j) < grid[i][j]) {
-          columnMap.set(j, grid[i][j])
-        }
-      } else {
-        columnMap.set(j, grid[i][j])
-      }
+      rowMap[i] = Math.max(rowMap[i], grid[i][j])
+      columnMap[j] = Math.max(columnMap[j], grid[i][j])
     }
   }
 
   for (let i = 0; i < grid.length; i++) {
     for (let j = 0; j < grid[0].length; j++) {
-      let current = grid[i][j]
-      if (current >= rowMap.get(i) && current >= columnMap.get(j)) {
-        continue
-      } else if (rowMap.get(i) <= columnMap.get(j)) {
-        output += rowMap.get(i) - current
-      } else {
-        output += columnMap.get(j) - current
-      }
+      output += Math.min(rowMap[i], columnMap[j]) - grid[i][j]
     }
   }
 
